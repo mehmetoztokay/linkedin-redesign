5dd7ae5e2d3513ce5584763acff9aa96fa16bdfe
Mehmet Öztokay <mehmetoztokay@gmail.com>
2021-12-07 17:21:50 +0300

    Responsive duzenlemeler yapildi.

--- www/js/swiperjs/swipers.js
+++ www/js/swiperjs/swipers.js
@@ -16,14 +16,17 @@ var swiper = new Swiper(".profile-swiper .swiper-container", {
     0: {
       slidesPerView: 1,
       spaceBetween: 20,
+      slidesPerGroup: 1,
     },
     577: {
       slidesPerView: 2,
       spaceBetween: 10,
+      slidesPerGroup: 2,
     },
     768: {
       slidesPerView: 3,
       spaceBetween: 20,
+      slidesPerGroup: 3,
     },
   },
 });
